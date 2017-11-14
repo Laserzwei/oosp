68f81dd2f1d8a710546067286e39a28baab894a6
Laserzwei <thembaandcoproductions@gmx.de>
2017-11-14 22:37:38 +0100

    less console spam

--- mods/oosp/config/config.lua
+++ mods/oosp/config/config.lua
@@ -2,7 +2,7 @@ config = {}
 
 config.includePlayerProperty = false            -- When false oosp will not update sectors with playerproperty in it, since the vanilla game can handle that. Defult false.
 config.ignoreVersioncheck = true                -- Allows to check, if a
-config.debugLevel = 2
+config.debugLevel = 0
 --These affect Tradingposts and consumers
 config.consumptionTime = 86400                  -- Time in s until stock of sold goods is emptied. Default 86400(24h). Has to be >0
 config.consumptionTimeVariation = 0.1         -- Variance in time. default 0.1 (10%) range [0.0 - 0.9]
