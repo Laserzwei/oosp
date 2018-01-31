b2f7a4b53c81c885169db45ff2f69ff08f09d463
Laserzwei <thembaandcoproductions@gmx.de>
2018-01-31 13:19:21 +0100

    -- removed oosp for factories -- removed oosp not installed detection -- removed oosp for all consumers -- updated debugprint functionality -- reworked code for resourcetraders update -- removed playerscript handling the (Un-)Loading event -- removed the timesystem for accurate out-of-sector calculations -- removed oosProdLib since it's not required anymore -- remove shipyard oosp update

--- mods/oosp/config/oosp.lua
+++ mods/oosp/config/oosp.lua
@@ -1,24 +1,32 @@
+package.path = package.path .. ";data/scripts/lib/?.lua"
+require ("utility")
 config = {}
-
-config.includePlayerProperty = false            -- When false oosp will not update sectors with playerproperty in it, since the vanilla game can handle that. Defult false.
-config.ignoreVersioncheck = true                -- Allows to check, if a
+config.MOD = "OOSP"
+config.VERSION = "[0.9_95] "
 config.debugLevel = 0
---These affect Tradingposts and consumers
-config.consumptionTime = 86400                  -- Time in s until stock of sold goods is emptied. Default 86400(24h). Has to be >0
-config.consumptionTimeVariation = 0.1           -- Variance in time. default 0.1 (10%) range [0.0 - 0.9]. Has +- effect
---This only affects Tradingposts
-config.generationTime = 86400                   -- Time in s until Stock of bought goods is filled. Default 86400(24h). Has to be >0
-config.generationTimeVariation = 0.1            -- Variance in time. default 0.1 (10%) range [0.0 - 0.9]. Has +- effect
+
 --This affects resource depots
 config.ResourcefillTime = 86400                 -- Time in suntil the resource depot fills up. Default 86400 (24h). Has to be >0
-config.ResourceMax = 100000                     -- Max. Stock the resource depot can get through oosp. Default 100.000. Has to be >0
-config.ResourceVariation = 0.1                  -- Variance in max. Stock and max. resource added by oosp. Default 0.1 (10%) range [0.0 - 0.9]. Has +- effect
+config.ResourceMaxFactor = 10.0                 -- Defines the maximum Stock-INCRESE as a factor of 10.000, per loading. Has to be >0
+config.ResourceVariation = 0.1                  -- Variance in max. Stock and max. resource added by oosp. Default 0.1 (10%) range [0.0 - 1.0]. Has +- effect
 
---Turn on/off update of specific stationtypes
-config.includeTradingPosts = true               -- This includes planetarytradingposts
-config.includeConsumers = true                  -- This includes biotope, casino, equipmentdock, habitat, militaryoutpost, repairdock, researchstation, resistance outpost, scrapyard, shipyard-trading, mines
-config.includeFactories = true
-config.includeResourceDepots = true
-config.includeShipyards = true                  -- This is for shipyard building time
+config.debugPrint = function (debuglvl, msg, ...)
+    if debuglvl <= config.debugLevel then
+        local str = config.MOD..config.VERSION..msg
+        local strs = {}
+        local tabls = {}
+        for _,arg in ipairs({...}) do
+            if type(arg) == "table" then
+                table.insert(tabls, arg)
+            else
+                table.insert(strs, arg)
+            end
+        end
+        for _,table in ipairs(tabls) do
+            printTable(table)
+        end
+        print(str, unpack(strs))
+    end
+end
 
 return config
